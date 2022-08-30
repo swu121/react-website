41a87b045a6f20cee05f1f3e5471749b7e3d4e5c
Steven Wu <swu121@gmail.com>
2022-08-29 19:01:28 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { ProjectSection } from './components/ProjectCard/projectsection';
-import { Header } from '../src/components/Header'
+import { Footer } from './components/Footer/footer'
 import './App.css';
 import { Resume } from './components/Resume/resume';
 import { AboutMe } from './components/AboutMe/aboutme'
 import {IconList} from './components/ContactIcons/iconlist'
 import background from './components/images/back.svg'
+
 function App() {
   return (
     <div className="App" style = {{backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', height: '100vh',  backgroundSize: 'cover', backgroundPosition: 'center'}}>
       <div className="IntroDesc">
-        <h1 style={{"--order": 1}} className="IntroText">Hey!</h1> 
+        <h1 style={{"--order": 1}} className="IntroText">Hey! &#128075;</h1> 
         <h1 style={{"--order": 2}} className="IntroText">I'm Steven Wu - a</h1>
         <h1 style={{"--order": 3}} className="IntroText">software engineer</h1>
         <h1 style={{"--order": 4}} className="IntroText">based in Portland, OR</h1>
@@ -18,6 +19,7 @@ function App() {
       <Resume/>
       <ProjectSection/>
       <AboutMe/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import "./footer.css"
+
+
+export function Footer(){
+    return(
+        <div className="main">
+            <h1 className='footertext' style={{fontSize: '35px'}}>Contact</h1>
+            <h2 className='footertext'>swu121@gmail.com || linkedin.com/in/swu121</h2>
+            <h3 className='footertext'>&copy; 2022</h3>
+        </div>
+    )
+}
